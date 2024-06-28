3c6ec812b06cb5dc6e6b4bb5dc56c41c99922c5c
matglima <mat.g.lima@protonmail.com>
2024-06-28 01:07:33 +0000

    changed setup.py to install requirements first

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 import os
 import subprocess
+import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install as _install
 
 def read_requirements():
     with open('requirements.txt') as req_file:
-        return req_file.readlines()
+        return req_file.read().splitlines()
 
-class post_install(_install):
+class PostInstallCommand(_install):
+    """Post-installation for installation mode."""
     def run(self):
         _install.run(self)
+        # Manually install requirements to ensure correct order
+        requirements = read_requirements()
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + requirements)
+        # Run the post-install script
         script_path = os.path.join(os.path.dirname(__file__), 'scripts/install_other_dependencies.sh')
         subprocess.check_call(['bash', script_path])
 
@@ -20,6 +26,6 @@ setup(
     package_dir={'': 'src'},
     install_requires=read_requirements(),
     cmdclass={
-        'install': post_install,
+        'install': PostInstallCommand,
     },
 )
